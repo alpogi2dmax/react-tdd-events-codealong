3934a1b8cecd89deb6a806ac990e810c168f2958
Al Cerdan Lico <alcerdan@yahoo.com>
2024-11-09 11:34:00 -0800

    complete tdd events codealong

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
+import { useState } from "react";
+
 function App() {
+
+  const[pepproniIsChecked, setPepproniIsChecked] = useState(false)
+
+  function togglePepperoni(e) {
+    setPepproniIsChecked(e.target.checked)
+  }
+
   return (
-    <div></div>
+    <div>
+      <h1>Select Pizza Toppings</h1>
+      <input
+        type='checkbox'
+        id='pepperoni'
+        checked={pepproniIsChecked}
+        aria-checked={pepproniIsChecked}
+        onChange={togglePepperoni}
+      />
+      <label htmlFor='pepperoni'>Add pepperoni</label>
+      <h2>Your Toppings:</h2>
+      <ul>
+        <li>Cheese</li>
+        {pepproniIsChecked ? <li>Pepperoni</li> : null}
+
+      </ul>
+    </div>
   )
 }
 
